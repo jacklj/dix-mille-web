2587700df5e4887cc3b589d210878b7e8b97461a
Jack Lawrence-Jones <jackvlj@gmail.com>
2020-06-21 01:47:11 +0100

    Set up firebase hosting, install basic packages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as firebase from 'firebase/app';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD96WTH3vOmrvdM9ACsQnW8XtamQAICLpg',
+  authDomain: 'dix-mille-lj.firebaseapp.com',
+  databaseURL: 'https://dix-mille-lj.firebaseio.com',
+  projectId: 'dix-mille-lj',
+  storageBucket: 'dix-mille-lj.appspot.com',
+  messagingSenderId: '460205070103',
+  appId: '1:460205070103:web:35c8e07bf97913d7adc952',
+  measurementId: 'G-R6TN7MHTGZ',
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
