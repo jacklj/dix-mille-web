bf415614b74ea777c3fc93b5d883e92902d0f098
Jack Lawrence-Jones <jackvlj@gmail.com>
2020-08-10 01:09:32 +0100

    Remove code that forces button to remain the same width and height - font loading time makes this difficult

--- src/components/forms/Button.js
+++ src/components/forms/Button.js
@@ -32,42 +32,16 @@ const BasicButton = styled.button`
     border: 6px double #ffbf00;
     color: #ffbf00;
   }
-
-  ${({ height }) => height && `height: ${height}px;`}
-  ${({ width }) => width && `width: ${width}px;`}
 `;
 
 const Button = ({ loading, children, className, onClick, disabled }) => {
-  /* Capture the dimensions of the button before the loading happens
-  so it doesn’t change size when showing the loader */
-  const [width, setWidth] = React.useState(0);
-  const [height, setHeight] = React.useState(0);
-  const ref = React.useRef(null);
-
-  // Save the dimensions here
-  React.useEffect(
-    () => {
-      if (ref.current && ref.current.getBoundingClientRect().width) {
-        setWidth(ref.current.getBoundingClientRect().width);
-      }
-      if (ref.current && ref.current.getBoundingClientRect().height) {
-        setHeight(ref.current.getBoundingClientRect().height);
-      }
-    },
-    // children are a dep so dimensions are updated if initial contents change
-    [children],
-  );
-
   return (
     <BasicButton
-      // ref={ref}
       className={className}
       disabled={disabled || loading}
       isLoading={loading}
       isDisabled={disabled} // the `disabled` prop is to disable the button element, when loading or disabled. `isDisabled` is for conditional styling
-      onClick={onClick}
-      width={width}
-      height={height}>
+      onClick={onClick}>
       {loading ? <LoadingSpinner /> : children}
     </BasicButton>
   );
