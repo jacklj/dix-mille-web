b814335820dc6485852076b8eaa6b19f45dc1633
Jack Lawrence-Jones <jackvlj@gmail.com>
2020-08-25 00:01:44 +0100

    Win lose music

--- src/pages/GameScreen/WinnerOverlay/index.js
+++ src/pages/GameScreen/WinnerOverlay/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import useSound from 'use-sound';
 
 import ScoresTable from 'components/ScoresTable';
 import { Button } from 'components/forms';
 import Overlay from 'components/Overlay';
 import { selectHasSomeoneWon } from 'redux/game/selectors';
 import WinnerText from './WinnerText';
+import { selectIsSoundOn } from 'redux/settings/selectors';
+import winMusic from 'media/sounds/winMusic.mp3';
+import loseMusic from 'media/sounds/loseMusic.mp3';
 
 const TableContainer = styled.div`
   flex: 0;
@@ -34,6 +38,27 @@ const WinnerOverlay = () => {
     history.push('/');
     // todo unsubscribe from game subscriptions, clear store
   };
+
+  const isSoundOn = useSelector(selectIsSoundOn);
+
+  const [playWinMusic] = useSound(winMusic, {
+    soundEnabled: isSoundOn,
+  });
+  const [playLoseMusic] = useSound(loseMusic, {
+    soundEnabled: isSoundOn,
+  });
+
+  const previousHasSomeoneWon = useRef(hasSomeoneWon);
+  useEffect(() => {
+    if (!previousHasSomeoneWon.current && hasSomeoneWon) {
+      if (hasSomeoneWon.didIWin) {
+        playWinMusic();
+      } else {
+        playLoseMusic();
+      }
+    }
+    previousHasSomeoneWon.current = hasSomeoneWon;
+  }, [hasSomeoneWon, playLoseMusic, playWinMusic]);
 
   if (!hasSomeoneWon) {
     return null;
