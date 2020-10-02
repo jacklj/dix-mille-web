464d143407daf503fded5b9c2cf89b7baeaa8a30
Jack Lawrence-Jones <jackvlj@gmail.com>
2020-10-02 13:44:04 +0100

    Make dice in rules popover white (not red)

--- src/components/Header/RulesPopover.js
+++ src/components/Header/RulesPopover.js
@@ -53,9 +53,10 @@ const CustomDie = styled(Die)`
   // if one die, put it in middle of grid row
   &:nth-child(1):last-child {
     grid-area: b;
-    background-color: red;
   }
 `;
+
+const ExampleDie = (props) => <CustomDie isInGroup {...props} />;
 
 const RulesPopover = ({ hideRules }) => {
   return (
@@ -91,55 +92,55 @@ const RulesPopover = ({ hideRules }) => {
       </P>
       <ScoreDemo>
         <Dice>
-          <CustomDie id={5} key={5} value={5} />
+          <ExampleDie id={5} key={5} value={5} />
         </Dice>
         50
       </ScoreDemo>
       <ScoreDemo>
         <Dice>
-          <CustomDie id={1} key={1} value={1} />
+          <ExampleDie id={1} key={1} value={1} />
         </Dice>
         100
       </ScoreDemo>
       <ScoreDemo>
         <Dice>
-          <CustomDie id={1} key={1} value={2} />
-          <CustomDie id={2} key={2} value={2} />
-          <CustomDie id={3} key={3} value={2} />
+          <ExampleDie id={1} key={1} value={2} />
+          <ExampleDie id={2} key={2} value={2} />
+          <ExampleDie id={3} key={3} value={2} />
         </Dice>
         Three of a kind = that number in 100s, e.g. three 2s = 200. Except three
         1s is 1000.
       </ScoreDemo>
       <ScoreDemo>
         <Dice>
-          <CustomDie id={1} key={1} value={2} />
-          <CustomDie id={4} key={4} value={4} />
-          <CustomDie id={5} key={5} value={5} />
-          <CustomDie id={2} key={2} value={2} />
-          <CustomDie id={3} key={3} value={4} />
-          <CustomDie id={6} key={6} value={5} />
+          <ExampleDie id={1} key={1} value={2} />
+          <ExampleDie id={4} key={4} value={4} />
+          <ExampleDie id={5} key={5} value={5} />
+          <ExampleDie id={2} key={2} value={2} />
+          <ExampleDie id={3} key={3} value={4} />
+          <ExampleDie id={6} key={6} value={5} />
         </Dice>
         Three pairs = 1000
       </ScoreDemo>
       <ScoreDemo>
         <Dice>
-          <CustomDie id={1} key={1} value={1} />
-          <CustomDie id={2} key={2} value={2} />
-          <CustomDie id={3} key={3} value={3} />
-          <CustomDie id={4} key={4} value={4} />
-          <CustomDie id={5} key={5} value={5} />
-          <CustomDie id={6} key={6} value={6} />
+          <ExampleDie id={1} key={1} value={1} />
+          <ExampleDie id={2} key={2} value={2} />
+          <ExampleDie id={3} key={3} value={3} />
+          <ExampleDie id={4} key={4} value={4} />
+          <ExampleDie id={5} key={5} value={5} />
+          <ExampleDie id={6} key={6} value={6} />
         </Dice>
         Sequence of 1 to 6 = 1500
       </ScoreDemo>
       <ScoreDemo>
         <Dice>
-          <CustomDie id={1} key={1} value={4} />
-          <CustomDie id={2} key={2} value={4} />
-          <CustomDie id={3} key={3} value={4} />
-          <CustomDie id={4} key={4} value={4} />
-          <CustomDie id={5} key={5} value={4} />
-          <CustomDie id={6} key={6} value={4} />
+          <ExampleDie id={1} key={1} value={4} />
+          <ExampleDie id={2} key={2} value={4} />
+          <ExampleDie id={3} key={3} value={4} />
+          <ExampleDie id={4} key={4} value={4} />
+          <ExampleDie id={5} key={5} value={4} />
+          <ExampleDie id={6} key={6} value={4} />
         </Dice>
         Six of any number - you win the game!
       </ScoreDemo>
