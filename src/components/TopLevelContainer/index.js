051554e1a7b723b54bb833d6c65859f8cc4b170b
Jack Lawrence-Jones <jackvlj@gmail.com>
2020-10-06 14:45:41 +0100

    WIP stop overscrolling

--- src/components/TopLevelContainer/index.js
+++ src/components/TopLevelContainer/index.js
@@ -7,6 +7,11 @@ import cardTableSurface from './card-table-surface-1.jpeg';
 const Container = styled.div`
   text-align: center;
   height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+  position: fixed;
+  top: 0;
+  left: 0;
   overflow: hidden;
 
   display: flex;
@@ -15,16 +20,16 @@ const Container = styled.div`
   // padding-bottom: 20px;
   // background-color: #076324; // card table green
 
-  background-image: url("${cardTableSurface}");
+  background-image: url('${cardTableSurface}');
   background-repeat: no-repeat;
   background-attachment: fixed;
   background-size: cover;
 
-  // background image effectively has a black translucent overlay over it (using 
-  // background-blend-mode: multiply;). This is removed when you start the proper 
+  // background image effectively has a black translucent overlay over it (using
+  // background-blend-mode: multiply;). This is removed when you start the proper
   // game, so the table surface is fully unveiled.
 
-  background-color: rgba(0,0,0,.4); // Tint color
+  background-color: rgba(0, 0, 0, 0.4); // Tint color
   background-blend-mode: ${(props) =>
     props.route === '/gameScreen' ? 'normal' : 'multiply'};
 `;
