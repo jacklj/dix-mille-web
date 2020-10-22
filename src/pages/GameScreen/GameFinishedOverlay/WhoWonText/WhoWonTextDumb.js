94c9a4bcc0dcaa6d20ed320deb8c6b9f137ed7ca
Jack Lawrence-Jones <jackvlj@gmail.com>
2020-10-22 17:46:20 +0100

    Remove animated text shadows - fixes Webkit elastic scrolling bug

--- src/pages/GameScreen/GameFinishedOverlay/WhoWonText/WhoWonTextDumb.js
+++ src/pages/GameScreen/GameFinishedOverlay/WhoWonText/WhoWonTextDumb.js
@@ -4,18 +4,18 @@ import styled, { css, keyframes } from 'styled-components';
 const bounceLost = keyframes`
 100% {
   transform: translateY(-10px);
-  text-shadow: 0 1px 0 #155415, 0 2px 0 #155415, 0 3px 0 #155415, 0 4px 0 #155415,
-    0 5px 0 #155415, 0 6px 0 #155415, 0 7px 0 #155415, 0 8px 0 #155415, 0 9px 0 #155415,
-    0 50px 25px rgba(170, 169, 173, 0.5);
+  // text-shadow: 0 1px 0 #155415, 0 2px 0 #155415, 0 3px 0 #155415, 0 4px 0 #155415,
+  //   0 5px 0 #155415, 0 6px 0 #155415, 0 7px 0 #155415, 0 8px 0 #155415, 0 9px 0 #155415,
+  //   0 50px 25px rgba(170, 169, 173, 0.5);
 }
 `;
 
 const bounceWon = keyframes`
 100% {
   transform: translateY(-10px);
-  text-shadow: 0 1px 0 #7F6319, 0 2px 0 #7F6319, 0 3px 0 #7F6319, 0 4px 0 #7F6319,
-    0 5px 0 #7F6319, 0 6px 0 #7F6319, 0 7px 0 #7F6319, 0 8px 0 #7F6319, 0 9px 0 #7F6319,
-    0 50px 25px rgba(255, 220, 127, 0.5);
+  // text-shadow: 0 1px 0 #7F6319, 0 2px 0 #7F6319, 0 3px 0 #7F6319, 0 4px 0 #7F6319,
+  //   0 5px 0 #7F6319, 0 6px 0 #7F6319, 0 7px 0 #7F6319, 0 8px 0 #7F6319, 0 9px 0 #7F6319,
+  //   0 50px 25px rgba(255, 220, 127, 0.5);
 }
 `;
 
@@ -53,7 +53,7 @@ const Container = styled.div`
     ${(props) =>
       props.didIWin
         ? css`
-            animation: ${bounceWon} 0.3s ease 4 alternate;
+            animation: ${bounceWon} 0.3s ease infinite alternate;
 
             text-shadow: 0 1px 0 #7f6319, 0 2px 0 #7f6319, 0 3px 0 #7f6319,
               0 4px 0 #7f6319, 0 5px 0 #7f6319, 0 6px 0 transparent,
@@ -61,7 +61,7 @@ const Container = styled.div`
               0 10px 10px rgba(255, 220, 127, 0.7);
           `
         : css`
-            animation: ${bounceLost} 0.3s ease 4 alternate;
+            animation: ${bounceLost} 0.3s ease infinite alternate;
 
             text-shadow: 0 1px 0 #155415, 0 2px 0 #155415, 0 3px 0 #155415,
               0 4px 0 #155415, 0 5px 0 #155415, 0 6px 0 transparent,
