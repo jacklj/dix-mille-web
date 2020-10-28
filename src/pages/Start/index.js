03218c54963b65580087c8401b99c6ec21a9b72f
Jack Lawrence-Jones <jackvlj@gmail.com>
2020-10-28 22:55:15 +0000

    Add "click here to read the rules" to start page

--- src/pages/Start/index.js
+++ src/pages/Start/index.js
@@ -3,10 +3,13 @@ import styled from 'styled-components';
 import 'firebase/functions';
 import { useHistory } from 'react-router-dom';
 import 'firebase/functions';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Logo from './Logo';
 import { Button } from 'components/forms';
 import SetupScreenContainer from 'components/SetupScreenContainer';
+import CONSTANTS from 'services/constants';
+import { showOverlay } from 'redux/ui/slice';
 
 const IntroText = styled.div`
   margin-top: 40px;
@@ -16,14 +19,33 @@ const IntroText = styled.div`
   font-size: 1.5em;
 
   text-shadow: 0 8px 8px rgba(0, 0, 0, 0.5);
+
+  &:last-child {
+    margin-bottom: 0;
+  }
 `;
 
 const CustomButton = styled(Button)`
   margin-bottom: 40px;
 `;
 
+const LinkButton = styled.button`
+  background: none;
+  border: none;
+  padding: 0;
+  text-decoration: underline;
+  cursor: pointer;
+
+  color: #ffdc73;
+  font-size: 1em;
+  font-family: 'Playfair Display', serif;
+`;
+
 const Start = () => {
   const history = useHistory();
+  const dispatch = useDispatch();
+
+  const showRules = () => dispatch(showOverlay(CONSTANTS.OVERLAYS.RULES));
 
   // N.B. <div>s around buttons are to fix a bug in mobile Safari where the flex container
   // forces the buttons to be really short (ie squished vertically)
@@ -46,6 +68,12 @@ const Start = () => {
           Multiplayer
         </CustomButton>
       </div>
+      <IntroText>
+        <p>
+          Click <LinkButton onClick={showRules}>here</LinkButton> to read the
+          rules.
+        </p>
+      </IntroText>
     </SetupScreenContainer>
   );
 };
