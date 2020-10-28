4aff1419053a6a103b4a93712b8674577d42d80e
Jack Lawrence-Jones <jackvlj@gmail.com>
2020-10-28 22:49:04 +0000

    Improve rules

--- src/components/Overlays/RulesPopover.js
+++ src/components/Overlays/RulesPopover.js
@@ -17,6 +17,15 @@ const H2 = styled.h2`
   }
 `;
 
+const H3 = styled.h3`
+  font-family: Limelight;
+  font-size: 2em;
+  text-transform: uppercase;
+
+  margin-bottom: 15px;
+  color: rgb(180, 176, 85);
+`;
+
 const P = styled.p`
   color: white;
   max-width: 700px;
@@ -61,35 +70,14 @@ const ExampleDie = (props) => <CustomDie isInGroup {...props} />;
 const RulesPopover = ({ hideRules }) => {
   return (
     <Overlay closeButton hide={hideRules}>
-      <H2>Rules</H2>
+      <H2>Rules (multiplayer)</H2>
       <P>A game for 2 or more players, using 6 dice.</P>
-      <P>Object is to score exactly 10,000.</P>
       <P>
-        When it is your turn, throw the dice and bank at least one scoring die.
-        Either stick with the score, or throw the remaining unbanked dice again.
-      </P>
-      <P>
-        If your throw contains no scoring dice, you have "blapped" - your turn
-        immediately ends with a score of 0.
-      </P>
-      <P>If you've banked all six dice, you can rethrow them all again.</P>
-      <P>
-        If you've banked 5 dice, you may throw the 6th die twice, aiming to roll
-        a scoring value (1 or 5). If you succeed, add its score (100 or 50) to
-        your turn score so far, and then double the total. If you don't score
-        having thrown once, you must throw a second time - you can't at that
-        point choose to stick.
-      </P>
-      <P>
-        If you overshoot 10,000, you return to your previous score (before that
-        turn started).
+        The goal is to be the first player to score exactly 10,000. You get
+        there by thowing certain combinations of dice:
       </P>
 
-      <H2>Scoring Dice</H2>
-      <P>
-        (N.B. a scoring group must be made up of dice from one throw: dice from
-        separate throws can't be grouped together)
-      </P>
+      <H3 id="scoring-dice">Scoring Dice</H3>
       <ScoreDemo>
         <Dice>
           <ExampleDie id={5} key={5} value={5} />
@@ -142,8 +130,39 @@ const RulesPopover = ({ hideRules }) => {
           <ExampleDie id={5} key={5} value={4} />
           <ExampleDie id={6} key={6} value={4} />
         </Dice>
-        Six of any number - you win the game!
+        Six of any number - you insta-win the game!
       </ScoreDemo>
+
+      <P>
+        When it is your turn, throw the dice and bank at least one scoring die.
+        Either stick with the score, or throw the remaining unbanked dice again.
+      </P>
+      <P>
+        If your throw contains no scoring dice, you have "blapped" - your turn
+        immediately ends with a score of 0.
+      </P>
+      <P>If you've banked all six dice, you can rethrow them all again.</P>
+      <P>
+        A scoring group must be made up of dice from one throw: dice from
+        separate throws can't be grouped together.
+      </P>
+      <P>
+        If you've banked 5 dice, you may throw the 6th die twice, aiming to roll
+        a scoring value (1 or 5). If you succeed, add its score (100 or 50) to
+        your turn score so far, and then double the total. If you don't score
+        having thrown once, you must throw a second time - you can't at that
+        point choose to stick.
+      </P>
+      <P>
+        If you overshoot 10,000, you return to your previous score (before that
+        turn started).
+      </P>
+
+      <H2>Single player</H2>
+      <P>
+        In single player mode, the goal is to score as many points as possible
+        in 6 rounds.
+      </P>
     </Overlay>
   );
 };
