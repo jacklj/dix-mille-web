788109b2b5c4c6a99be1c6f8df0e9a887d01290e
Jack Lawrence-Jones <jackvlj@gmail.com>
2020-12-31 01:15:43 +0000

    Improve loading message style

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Loading = ({ className }) => <div>Loading...</div>;
+const Container = styled.div`
+  flex: 1;
+
+  z-index: 0; // establish stacking context for pages (so Overlay is always on top)
+
+  display: flex;
+`;
+
+const LoadingMessage = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 30%;
+`;
+const Loading = ({ className }) => (
+  <Container>
+    <LoadingMessage>Loading...</LoadingMessage>
+  </Container>
+);
 
 export default Loading;
